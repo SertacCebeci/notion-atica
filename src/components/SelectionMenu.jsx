c2c30c6500803c699a1c1f672aaae9ff86ba72f6
SertacCebeci <ali.sertac.cebeci.95@gmail.com>
2023-03-23 22:33:24 +0300

    refactor: select menu

--- src/components/SelectionMenu.jsx
+++ src/components/SelectionMenu.jsx
@@ -19,26 +19,38 @@ const SelectionMenu = ({ editor }) => {
           tippyOptions={{ duration: 10 }}
         >
           <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            className={`bg-white m-1 p-1 font-bold ${
-              editor.isActive("bold") ? "bg-gray-200" : "bg-white"
-            }`}
+            onClick={() => {
+              if (editor.isActive("bold")) {
+                editor.chain().focus().unsetBold().run();
+              } else {
+                editor.chain().focus().setBold().run();
+              }
+            }}
+            className="bg-white m-1 p-1 font-bold hover:cursor-pointer hover:bg-gray-200"
           >
             Bold
           </button>
           <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            className={`bg-white m-1 p-1 px-1 italic ${
-              editor.isActive("italic") ? "bg-gray-200" : "bg-white"
-            }`}
+            onClick={() => {
+              if (editor.isActive("italic")) {
+                editor.chain().focus().unsetItalic().run();
+              } else {
+                editor.chain().focus().setItalic().run();
+              }
+            }}
+            className="bg-white m-1 p-1 italic hover:cursor-pointer hover:bg-gray-200"
           >
             Italic
           </button>
           <button
-            onClick={() => editor.chain().focus().toggleUnderline().run()}
-            className={`bg-white m-1 p-1 underline ${
-              editor.isActive("underline") ? "bg-gray-200" : "bg-white"
-            }`}
+            onClick={() => {
+              if (editor.isActive("underline")) {
+                editor.chain().focus().unsetUnderline().run();
+              } else {
+                editor.chain().focus().setUnderline().run();
+              }
+            }}
+            className="bg-white m-1 p-1 underline hover:cursor-pointer hover:bg-gray-200"
           >
             Underline
           </button>
