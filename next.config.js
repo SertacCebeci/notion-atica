44ba792c249e646768292dc5b6bd3016c9e95a34
SertacCebeci <ali.sertac.cebeci.95@gmail.com>
2023-03-26 18:57:33 +0300

    vercel fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["https://notion-atica.vercel.app"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
